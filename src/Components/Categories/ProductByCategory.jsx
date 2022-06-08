3710d1a13fa8bf8baa01ecbf7ef55450e1894495
Max Lythgoe <lythgoemax@gmail.com>
2022-06-08 11:11:22 -0600

    added product filters in category section

--- src/Components/Categories/ProductByCategory.jsx
+++ src/Components/Categories/ProductByCategory.jsx
@@ -9,6 +9,7 @@ import Categories from './Categories';
 function ProductByCategory({update, setUpdate}) {
     const [data, setData] = useState([])
     const [catname, setCatname] = useState('phones')
+    const [filter, setFilter] = useState('alph')
 
     const addToCart = (id) => {
         let object = {
@@ -22,15 +23,26 @@ function ProductByCategory({update, setUpdate}) {
     useEffect(() => {
         axios.get(`http://localhost:4000/api/categories/${catname}`)
         .then((res) => {
-            setData(res.data)
-        })
-    }, [catname])
+            if (filter === 'low') {
+            setData(res.data.sort((a, b) => a.price - b.price))
+        } else if (filter === 'high') {
+            setData(res.data.sort((a, b) => b.price - a.price))
+        } else if (filter === 'alph') {
+            setData(res.data.sort((a, b) => a.name.localeCompare(b.name)))
+        }})
+    }, [catname, filter])
 
     return (
         <div>
             <Categories setCatname={setCatname} />
             <h2>Products</h2>
             <hr className="line"></hr>
+            <label className='sort'>Sort By:</label>
+            <select className="selector" onChange={(event) => {setFilter(event.target.value)}} name="price-sort">
+                <option value="low">Price: Low-to-High</option>
+                <option value="high">Price: High-to-Low</option>
+                <option value="alph">Product Name: A-Z</option>
+            </select>
             <div className='products-container'>
             {data.map((element,index) => {
                 return <ProductCard data={element} key={index} addToCart={addToCart} />
